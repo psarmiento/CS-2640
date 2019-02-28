a330a21635caf1f541542220cca29f3372f11994
psarmiento <2010sarmiento@gmail.com>
2019-02-27 21:37:59 -0800

    Update one_per_line.asm

--- one_per_line.asm
+++ one_per_line.asm
@@ -1,61 +1,68 @@
 .data
-array:	.space		24			# array big enough to hold 20 integers (4 bytes each)
-prompt:	.asciiz 	"Enter an integer: " 	
-newLine: .asciiz	"\n "
+array:		.space	40
+array_size:	.word	20	
+prompt:		.asciiz "Enter an integer: "
+newLine:	.asciiz "\n"
+test:		.asciiz "Test"
 
 .text
 
 main:
-la $t0, array 		# first loading address into t0 
+la $t0, array
+la $t1, array_size	# holds address in t1 (load address into t1)
+lw $t1, 0($t1)		# loading actual value into t1 w/ 0 offset t1 = 10 
+ 				# lw (load word) gets the actual value from a certain address
+sll  $t1, $t1, 2
+addu $t1, $t0, $t1
+la $t3, prompt		# holds address for prompt
+ 	
+input_loop:
+slt $t2, $t0, $t1
+beq $t2, $0, exit_input_loop		# branch if equal, compare value in t2 to 0
+# prompt for int input 
+move $a0, $t3	# moves t3 into a0
+li $v0, 4	# prepares system to output t3
+syscall		# does the actual output 
+# get input
+li $v0, 5	# prepares system to get input 
+syscall
+ 	
+sw $v0, 0($t0)	# store int 
+ 	
+addiu $t0, $t0, 4	# increment iterator 
+ 	
+j input_loop 			# go to loop
+ 	
+exit_input_loop:
+ 	
+# input loop portion 
+addi $t1, $zero, 0
+addi $t2, $zero, 0	# set t2 to zero (incrementor for loop)
 
-#la $t1, prompt		# loading prompt into t1
-addi $t1, $zero, 0	# incrementor for loop 
-addi $t3, $zero, 0	# offset for storing into array 
-loop:
-	beq $t1, 20, exit_loop		# increment by 1, if t1 == 20 exit loop
-	
-	la $a0, prompt			# print prompt for user 
-	li $v0, 4
-	syscall
-	
-	li $v0, 5			# getting user input 
-	syscall	
-	
-	sw $v0, 0($t0) 
-	
-	addi $t3, $t3, 4		# increment offset for array by 4
-	addi $t1, $t1, 1
-	j loop	# jumps back to top of loop 
-exit_loop:
- 
-lw $t4, array($t3)
-move $a0, $t4
+output:
+beq $t2, 20, output_exit
+
+lw $t4, array($t1)	# load value from array with offset at t1 into t4
 li $v0, 1
+move $a0, $t4
+syscall			# print array value 
+
+
+# print new line 
+li $v0, 4
+la $a0, newLine
 syscall
 
-# reset t1 to 0 in order to increment through this array 
-addi $t1, $zero, 0
-# also reset offest for array 
-addi $t3, $zero, 0
-print_loop:
-	beq $t1, 20, exit
-	
-	lw $t4, array($t1)
-	li $v0, 1
-	move $a0, $t4
-	syscall 
-	
-	la $a0, newLine		# create new line 
-	li $v0, 4
-	syscall
-	
-	addi $t1, $t1, 1		# increment loop iterator 
-	addi $t3, $t3, 4		# increment offset for array 
-	
-	j print_loop
+# test 
+li $v0, 4
+la $a0, test
+syscall
 
+addi $t2, $t2, 1	# update value for incrementor by 1
+addi $t1, $t1, 4	# update value for offset by 4
 
-exit_print:
+j output	# jump back to output
+output_exit:
 
 exit:
 li $v0, 10 

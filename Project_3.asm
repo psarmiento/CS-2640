c292fe91e7bd6bf8a9e83a7bd4febdd1d787c46e
psarmiento <2010sarmiento@gmail.com>
2019-03-25 20:20:28 -0700

    Update Project_3.asm

--- Project_3.asm
+++ Project_3.asm
@@ -4,7 +4,7 @@
 # TO DO LIST: 1) Work on sorting function, making sure to properly store a value into array 
 .data
 # array to hold sorted integers in ascending order 
-array:					.space 160 
+array:					.space 200 
 # prompt for number of signed integers to input
 numberOfInput:				.asciiz "Enter the number of signed integers to enter: " 
 # prompt for user to enter an integer
@@ -82,39 +82,36 @@ loadInput:				.word 4
 # a1 = address of array 
 # a2 = user input being inserted 
 insertArr:
-addi $sp, $sp, -4				# allocate some space for s0 on the stack 
-sw $s0, 0($sp)					# save s0 on stack 
-move $t1, $a2					# move user input (a2) into t1
-addi $a1, $zero, 0				# start at the beginning of the loop
-mul $a1, $a0, 4					# a1 = s0 * 4
-addi $a1, $a1, -4				# properly align a1 
-move $s1, $a1					# temporarily store a1 into s1 for case where
+addi $sp, $sp, -12				# allocate some space for local variables on the stack 
+move $s0, $a0					# move number of inputs into s0
+move $s1, $a1					# move array address into s1
+move $s2, $a2					# move user input into s2
+sw $s0, 0($sp)					# save s0 on stack (number of input)
+sw $s1, 0($sp)
+sw $s2, 0($sp)					# save s2 on stack 
+
+addi $s1, $zero, 0				# start at the beginning of the loop
+mul $s1, $s0, 4					# s1 = s0 * 4
+addi $s1, $s1, -4				# properly align s1 
 						# inserted element goes at end of array 
 	# this loop will find the proper place for user's input to be inserted into 
 	# all elements will first be shifted and then compared to see if the value 
 	# is in the proper place 
 	insert:
 	#beq $t2, 0, exit_insert		# if t2 = 0, break from loop 
-	lw $t3, array($a1)			# load value in array into t3
-	slt $t2, $t3, $t1			# if (t3 < t1) then insert t1 into array and exit loop 
+	lw $t3, array($s1)			# load first value in array into t3
+	slt $t2, $t3, $s2			# if (t3 < t1) then insert t1 into array and exit loop 
 	beq $t2, 1, pos_found			# branch if t2 == 1
-	addi $a1, $a1, 4			# shift a1 into the next position, and store the array value
-	sw $t3, 0($a1)
-	addi $a1, $a1, -4			# move a1 back to original position 
-	sw $a2, 0($t1)				# temporarily store user input into array 
-	addi $a1, $a1, -4			# increment a1 by -4
+	addi $s1, $s1, 4			# shift a1 into the next position, and store the array value
+	sw $t3, array($s1)
+	addi $s1, $s1, -4			# move a1 back to original position 
+	sw $s2, array($s1)			# temporarily store user input into array 
+	addi $s1, $s1, -4			# increment a1 by -4
 	j insert
 	exit_insert:
 	
 	pos_found:
-	beq $a1, $s1, last_Pos			# if the element is being inserted into the last position in array
-						# then break, else jump back to main
-	addi $sp, $sp, 4			# unload off stack
+	sw $s2, array($s1)
+	addi $sp, $sp, 12
 	jr $ra
 	
-	last_Pos:
-	#addi $a1, $a1, 4
-	move $t4, $a1
-	sw $a2, 0($t4)				# store t1 (user input) into last element of array
-	addi $sp, $sp, 4
-	jr $ra
